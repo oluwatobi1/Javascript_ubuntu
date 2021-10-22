f0a899750c5ba9f223cea95fa4ce9c7338cebecc
Tobi Akin <akintunlesetobi@gmail.com>
2021-10-23 00:53:07 +0100

    control whti eelsee if

---
+++ control2.js
@@ -0,0 +1,11 @@
+var age = 54
+
+if (age>30){
+    document.write("Age is below ten")
+} else if (age>20){
+    document.write("This is above 20")
+} else if (age>10){
+    document.write("This is above 10")
+}else {
+    document.write("This is above 20")
+}
