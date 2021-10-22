c1409b8c22da54686335c79ad9acc51d94b7a363
Tobi Akin <akintunlesetobi@gmail.com>
2021-10-22 22:33:37 +0100

    first commit

---
+++ control.js
@@ -0,0 +1,8 @@
+var youLikeMeat = true
+
+if (youLikeMeat){
+    document.write("You like meat...")
+    document.write("Here is the meat menu...")
+} else {
+    document.write("You don't like meat")
+}
