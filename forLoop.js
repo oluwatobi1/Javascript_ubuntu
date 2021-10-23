3908331a1187ab27b515dcdf5afa743835465036
Tobi Akin <akintunlesetobi@gmail.com>
2021-10-23 13:41:50 +0100

    for loops

---
+++ forLoop.js
@@ -0,0 +1,5 @@
+var links = document.getElementsByTagName("a")
+
+for (i = 0; i<links.length;i++){
+    console.log(links);
+}
---
+++ while.js
@@ -0,0 +1,7 @@
+var currAge = 23
+
+while (currAge<30){
+    console.log("current ag", currAge);
+    document.write("this is current age", currAge)
+    currAge++;
+}
