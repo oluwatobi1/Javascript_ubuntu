241448259d51b4cc94f6592e264858917128885e
Tobi Akin <akintunlesetobi@gmail.com>
2021-10-23 14:36:00 +0100

    functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+function getAverage(a,b) {
+    var average = (a + b )/2
+    console.log("average ", average);
+
+    return average
+}
+
+var myResult = getAverage(4,56)
+console.log("Result ", myResult);
