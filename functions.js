fca794759bf589d9d7ed9744fe1088e0b9d54096
Tobi Akin <akintunlesetobi@gmail.com>
2021-10-23 14:59:15 +0100

    nan

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
 function getAverage(a,b) {
-    var average = (a + b )/2
+    var average = (a + b )/2 // local variable
     console.log("average ", average);
 
     return average
 }
 
-var myResult = getAverage(4,56)
+var myResult = getAverage(4,56) // global variable
 console.log("Result ", myResult);
---
+++ nan.js
@@ -0,0 +1,8 @@
+var  a = 84
+var b = 6
+
+if (isNaN(a)){
+    console.log(a+" is NOT a number");
+} else {
+    console.log(a +' is a  number');
+}
