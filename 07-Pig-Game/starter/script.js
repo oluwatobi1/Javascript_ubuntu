5468e71340eb4c3585b99fc07cc297925d0c2d3a
oluwatobi1 <akintunlesetobi@gmail.com>
2021-12-09 16:29:57 +0100

    task:item selections

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,12 @@
-'use strict';
+var diceNumber, activePlayer, scores, currentScore
+
+scores = [0, 0]
+activePlayer = 0;
+currentScore = 0;
+
+diceNumber = Math.floor(Math.random() * 6) + 1
+document.querySelector('#current--' + activePlayer).textContent = diceNumber
+
+var diceImage = document.querySelector('.dice')
+
+diceImage.style.display = 'none'
