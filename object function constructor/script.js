cad23244fd52b2c82586cc6b4a4e1e1e7634264d
oluwatobi1 <akintunlesetobi@gmail.com>
2021-12-13 15:29:54 +0100

    objects

---
+++ object function constructor/script.js
@@ -0,0 +1,10 @@
+var anne = {
+    name: 'anne',
+    yearOfBirth: 1989,
+    location: 'imo',
+    calculateAge: function() {
+        console.log(2020 - this.yearOfBirth);
+    }
+}
+
+anne.calculateAge();
