268e19fc7f2b3ef4f6fd4b9b949e032da6c38b66
oluwatobi1 <akintunlesetobi@gmail.com>
2021-12-14 16:18:20 +0100

    prototype

--- object function constructor/script.js
+++ object function constructor/script.js
@@ -8,3 +8,20 @@ var anne = {
 }
 
 anne.calculateAge();
+
+var Person = function(name, yearOfBirth, location) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.location = location;
+}
+Person.prototype.calculateAge = function() {
+    console.log(2020 - this.yearOfBirth);
+}
+
+var john = new Person('john', 1993, 'abuja')
+console.log(john);
+
+john.calculateAge();
+
+var dill = new Person('dill', 1943, "castlevania")
+dill.calculateAge()
