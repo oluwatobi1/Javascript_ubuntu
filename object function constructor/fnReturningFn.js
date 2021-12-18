6c0dff6bdade781fbbb1a126889299d2cf41f50f
oluwatobi1 <akintunlesetobi@gmail.com>
2021-12-18 23:34:07 +0100

    function returning fn

--- object function constructor/fnReturningFn.js
+++ object function constructor/fnReturningFn.js
@@ -1,1 +1,15 @@
-console.log("fn returning fn");
+function interviewQuestion(job) {
+    if (job === "designer") {
+        return function(name) {
+            console.log(name, " how many years have you been a designer");
+        }
+    } else if (job === "teacher") {
+        return function(name) {
+            console.log(name, " do you like teaching?");
+        }
+    } else {
+        return function(name) {
+            console.log("Hey ", name, " what do you do?.");
+        }
+    }
+}
