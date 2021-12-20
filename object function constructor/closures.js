bb05ac48102c75023798820087b53ce3fb10d3e3
oluwatobi1 <akintunlesetobi@gmail.com>
2021-12-20 11:20:05 +0100

    closure

---
+++ object function constructor/closures.js
@@ -0,0 +1,40 @@
+// Closure: an inner function always has access to the variable declared in its outer function 
+// even when the function is returned
+
+function isOfAge(retirement) {
+    var a = " years left"
+    return function(yearOfBirth) {
+        var age = 2021 - yearOfBirth
+        return retirement - age;
+    };
+};
+
+var john = isOfAge(60);
+console.log(john(1998));
+
+function interviewQuestion(job) {
+    var a = " what do designers do?"
+    var b = " how long have you been teaching?"
+    var c = " what is your occupation?"
+
+    if (job === "designer") {
+        return function(name) {
+            console.log(name + a);
+        };
+    } else if (job === "teacher") {
+        return function(name) {
+            console.log(name + b);
+        };
+    } else {
+        return function(name) {
+            console.log(name + c);
+        };
+    };
+};
+
+
+var dillDoe = interviewQuestion('teacher')
+dillDoe('dill Doe')
+
+var annePosition = interviewQuestion('designer')
+annePosition("Annie Position")
