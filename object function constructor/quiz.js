cc4fcc174c46fbc1e98b3fb146fe79ca8949ded7
oluwatobi1 <akintunlesetobi@gmail.com>
2021-12-28 22:27:55 +0100

    fixed score bug

--- object function constructor/quiz.js
+++ object function constructor/quiz.js
@@ -22,29 +22,49 @@ var thirdQuestion = new Question("Who is the minister of power", {
         3: "Babatunde Raji Fashola"
     },
     3)
+var fourthQuestion = new Question("Who is the minister of communication", {
+        1: "Layiwola Lai Mohammed",
+        2: "Mallam Adamu Adamu",
+        3: "Babatunde Raji Fashola"
+    },
+    3)
+var fifthQuestion = new Question("Who is the minister of health", {
+        1: "Layiwola Lai Mohammed",
+        2: "Mallam Adamu Adamu",
+        3: "Babatunde Raji Fashola"
+    },
+    3)
+var sixthQuestion = new Question("Who is the minister of information", {
+    1: "Layiwola Lai Mohammed",
+    2: "Mallam Adamu Adamu",
+    3: "Babatunde Raji Fashola"
+}, 3)
 
-var questionList = [firstQuestion, secondQuestion, thirdQuestion]
+var questionList = [firstQuestion, secondQuestion, thirdQuestion, fourthQuestion, fifthQuestion, sixthQuestion]
 var score = 0;
-
-
+console.log("Starting... ");
 
 
 function game() {
     var questionNumber = Math.floor(Math.random() * 3);
     var currentQuestion = questionList[questionNumber];
     console.log(currentQuestion.question);
-    console.log(currentQuestion.options);
+    for (let item in currentQuestion.options) {
+        console.log(item, ":", currentQuestion.options[item])
+    }
+    console.log("this is an expo:", currentQuestion.answer);
     console.log("Your Score is ", score);
+
     var answer = prompt(currentQuestion.question);
 
     function increaseScore() {
-        score = score++
+        score++
     };
     if (answer === "exit") {
         return
     } else if (answer == currentQuestion.answer) {
         increaseScore()
-        console.log("Your Score is ", score);
+        console.log(" Correct... Your Score is ", score);
         console.log("|----------------|");
         game();
     } else {
